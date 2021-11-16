3bdc20d15a682ae15b753aea30dad64b2bd7d970
GusttavoTorres <88936210+GustavoRT-debug@users.noreply.github.com>
2021-11-16 18:03:42 -0300

    Update topo-2sw-2host.py

--- topo-2sw-2host.py
+++ topo-2sw-2host.py
@@ -10,14 +10,27 @@ class MyTopo( Topo ):
         Topo.__init__( self )
 
         # Add hosts and switches
-        leftHost = self.addHost( 'h1' )
-        rightHost = self.addHost( 'h2' )
-        leftSwitch = self.addSwitch( 's3' )
-        rightSwitch = self.addSwitch( 's4' )
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        h3 = self.addHost('h3')
+        h4 = self.addHost('h4')
+        h5 = self.addHost('h5')
+        h6 = self.addHost('h6')
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
 
         # Add links
-        self.addLink( leftHost, leftSwitch )
-        self.addLink( rightHost, rightSwitch )
-        self.addLink( leftSwitch, rightSwitch )
+        self.addLink( h1, s1)
+        self.addLink( h2, s1)
+        self.addLink( h6, s2)
+        self.addLink( h5, s2)
+        self.addLink( h3, s3)
+        self.addLink( h4, s3)
+        self.addLink( s1, s2)
+        self.addLink( s2, s3)
+       
+
+topos = {'mytopo': (lambda : MyTopo())}
 
 topos = { 'mytopo': ( lambda: MyTopo() ) }
